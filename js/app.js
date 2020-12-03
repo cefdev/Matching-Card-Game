1a15233c76dd4d31bef0b3237ebcc31173ac2a2e
Moncef <moncef.koumila@gmail.com>
2020-12-03 20:28:56 +0000

    Functionality for when a card is clicked

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ function init() {
     icons.push(card.children[0].className);
 
     // If card clicked
-    //card.addEventListener("click", cardClicked);
+    card.addEventListener("click", cardClicked);
   });
 
   // Shuffle cards
@@ -67,7 +67,26 @@ function shuffleCards() {
   });
 }
 
-// cardClicked(): Do some logic when a card is being clicked
+// Do some logic when a card is being clicked
+function cardClicked() {
+  // Start the timer
+  startTimer();
+
+  // Push the clicked card into the openedCards array if:
+  if (openedCards.length < 2 && !this.classList.contains("show")) {
+    this.classList.add("open");
+
+    // Push the opened card into the openedCards array
+    openedCards.push(this);
+
+    // if openedCard length == 2:
+    if (openedCards.length == 2) {
+      // run checkMatch function after 0.7 seconds to let the user see the second clicked card icon clearly before it flip down
+      // setTimeout(checkMatch, 700);
+    }
+  }
+}
+
 // checkMatch(): Check if the two opened cards are matched or not
 // updateScore(): update the score, the stars rating
 
@@ -75,15 +94,15 @@ function shuffleCards() {
 function startTimer() {
   if (!timerhandler) {
     timerhandler = setInterval(() => {
-      second += 1;
-      if (second > 59) {
-        second = 0;
-        minute += 1;
+      seconds += 1;
+      if (seconds > 59) {
+        seconds = 0;
+        minutes += 1;
       }
 
-      second < 10
-        ? (timer.innerHTML = `0${minute}:0${second}`)
-        : (timer.innerHTML = `0${minute}:${second}`);
+      seconds < 10
+        ? (timer.innerHTML = `0${minutes}:0${seconds}`)
+        : (timer.innerHTML = `0${minutes}:${seconds}`);
     }, 1000);
   }
 }
