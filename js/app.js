bd4d3a9d83b5244477293ab4eeb0e69658a41c56
Moncef <moncef.koumila@gmail.com>
2020-12-03 19:42:20 +0000

    Build init()

---
+++ js/app.js
@@ -0,0 +1,66 @@
+/* Initial Variables */
+let cardList = document.querySelectorAll(".card");
+let movesElement = document.querySelector(".moves");
+let starsElement = document.querySelector(".stars");
+let winningPanel = document.querySelector(".winning-panel");
+let restartBtn = document.querySelector(".restart");
+let timer = document.querySelector(".timer");
+let icons = [];
+let openedCards = [];
+let numOfMoves = 0;
+let numOfStars = 3;
+let seconds = 0;
+let minutes = 0;
+let timerhandler;
+
+// Initial function (Collect cards icons, Flip cards down, Shuffle cards, Reset timer and score). Run in the begining of the game.
+function init() {
+  // Reset icons to empty array
+  icons = [];
+
+  // Display 3 stars
+  starsElement.innerHTML =
+    '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+
+  // Change Moves Panel value to 0
+  numOfMoves = 0;
+  movesElement.textContent = numOfMoves;
+
+  // Reset timer
+  seconds = 0;
+  minutes = 0;
+  timer.textContent = "00:00";
+
+  // Change restart button color to white
+  restartBtn.style.color = "white";
+
+  // Hide winning Panel
+  winningPanel.style.display = "none";
+
+  // Display cards on the page
+  cardList.forEach((card) => {
+    // Face down all cards
+    card.classList.remove("open");
+    card.classList.remove("match");
+
+    // Collect available icons names
+    icons.push(card.children[0].className);
+
+    // Shuffle cards
+    shuffleCards();
+
+    // If card clicked
+    card.addEventListener("click", cardClicked);
+  });
+}
+
+// cardClicked(): Do some logic when a card is being clicked
+// checkMatch(): Check if the two opened cards are matched or not
+// updateScore(): update the score, the stars rating
+// startTimer()
+// stopTimer()
+// openWinPanel(): Display the winning panel
+// Logic to restart the game after winning or from the panel (Using Restart button)
+
+// Run init function in the begining of the game
+window.addEventListener("load", init);
